681b1980bb0c7227fcd5d1f51bbee34e3f9f7e09
erma0x <ermano.buikis@gmail.com>
2022-02-21 23:50:15 +0100

    update

--- src/components/NewQuizButton.vue
+++ src/components/NewQuizButton.vue
@@ -1,21 +1,44 @@
 <template>
-  <div class="NewQuizButton">
-    <!-- <button id="NewQuizButton" name="NewQuizButton">New Quiz</button> -->
-    <button class="btn btn-primary" type="submit">New Quiz</button>
-
-
+  <div id="NewQuizButton" class="new-quiz-button">
+    <button v-on:click="getNewQuiz()" class="btn btn-primary">
+      New Quiz
+    </button>
   </div>
 </template>
 
 <script>
 export default {
+  el: "NewQuizButton",
 
-  
+  data() {
+    return {
+      randomTest:{
+        name:'randomTest',
+        default:this.getNewQuiz(),
+      },
+    };
+  },
+  methods: {
+    async getNewQuiz() {
+      const url = "http://localhost:3001/quiz";
+      let response = await fetch(url, { method: "GET" });
+      let randomTest = await response.json();
+      console.log(randomTest)
+      this.$emit('myEvent')
+      return randomTest; //this$emit
+    },
+  },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+div {
+  margin: 10px 0 0;
+  padding: 5;
+}
+button {
+  margin: 10px 0 0;
+}
 
 h3 {
   margin: 40px 0 0;
@@ -26,7 +49,7 @@ ul {
 }
 li {
   display: inline-block;
-  margin: 0 10px;
+  margin: 0 0px;
 }
 .NewQuizButton {
   color: #42b983;
